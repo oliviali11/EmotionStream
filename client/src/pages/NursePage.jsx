7bc532c677bb9d951c031f4fc03f4b7c6fbe1e80
Olivia Li <artsyoli11@gmail.com>
2024-06-22 17:46:32 -0700

    Commit message describing your changes

--- client/src/pages/NursePage.jsx
+++ client/src/pages/NursePage.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
-const NursePage = () => {
+const NursePage = ({ negativeDetected }) => {
   return (
     <>
     <h1>Patients</h1>
+    {negativeDetected ? (
+        <div className="alert alert-info" role="alert">
+          Predictions received!
+          {/* <ul>
+            {predictions.map((prediction, index) => (
+              <li key={index}>{prediction}</li>
+            ))}
+          </ul> */}
+        </div>
+      ) : (
+        <p>No predictions received yet.</p>
+      )}
     </>
   )
 }
